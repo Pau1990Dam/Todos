6fdd15e16442d162cdfd742729be821ae9a6b144
lawer <carlesgm@gmail.com>
2015-11-16 22:40:30 +0100

    Removed detail fab

--- app/src/main/java/org/ecaib/todos/DetailActivityFragment.java
+++ app/src/main/java/org/ecaib/todos/DetailActivityFragment.java
@@ -1,25 +1,15 @@
 package org.ecaib.todos;
 
-import android.database.Cursor;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-
-import org.ecaib.todos.provider.notes.NotesColumns;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class DetailActivityFragment extends Fragment {
-
-    private SimpleCursorAdapter adapter;
 
     public DetailActivityFragment() {
     }
