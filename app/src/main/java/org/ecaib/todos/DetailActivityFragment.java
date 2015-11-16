feb887331e2011914631e55e6653e4e58c5dbc25
lawer <carlesgm@gmail.com>
2015-11-16 22:10:13 +0100

    SimpleCursorAdapter

--- app/src/main/java/org/ecaib/todos/DetailActivityFragment.java
+++ app/src/main/java/org/ecaib/todos/DetailActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +21,21 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        ListView lvTodos = (ListView) view.findViewById(R.id.toolbar);
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                getContext(),
+                R.layout.notes_row,
+                null,
+                new String[] { "title" },
+                new int[] { R.id.tvTitle },
+                0
+        );
+
+        lvTodos.setAdapter(adapter);
+
+        return view;
     }
 }
