f5e8dceafd76a161eed84d16683f00f98c404617
lawer <carlesgm@gmail.com>
2015-11-16 22:52:09 +0100

    Menu on detailactivity

--- app/src/main/java/org/ecaib/todos/DetailActivity.java
+++ app/src/main/java/org/ecaib/todos/DetailActivity.java
@@ -16,13 +16,6 @@ public class DetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
--- app/src/main/java/org/ecaib/todos/DetailActivityFragment.java
+++ app/src/main/java/org/ecaib/todos/DetailActivityFragment.java
@@ -3,6 +3,8 @@ package org.ecaib.todos;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -15,6 +17,13 @@ public class DetailActivityFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_detail, container, false);
@@ -22,4 +31,11 @@ public class DetailActivityFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_detail, menu);
+
+    }
 }
